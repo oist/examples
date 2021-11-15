f3b78f56f55e746f2fe8bfdfe4f12efe51349ebb
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2021-11-15 18:55:20 +0900

    wip working on approach after puck goes out of sight. Not working

--- lite/examples/object_detection/android/app/src/main/java/org/tensorflow/lite/examples/detection/CenterPuckController.java
+++ lite/examples/object_detection/android/app/src/main/java/org/tensorflow/lite/examples/detection/CenterPuckController.java
@@ -1,9 +1,7 @@
 package org.tensorflow.lite.examples.detection;
 
-import android.graphics.Point;
 import android.util.Log;
 
-import java.util.List;
 import java.util.Random;
 
 import jp.oist.abcvlib.core.outputs.AbcvlibController;
@@ -15,20 +13,26 @@ public class CenterPuckController extends AbcvlibController {
     private float p_phi = 0.35f;
     private boolean targetAquired = false;
     private float proximity = 0; // from 0 to 1 where 1 is directly in front of the camera and zero being invisible.
-    int noBlobInFrameCounter = 0;
-    int blobInFrameCounter = 0;
+    int noTargetInFrameCounter = 0;
+    int targetInFrameCounter = 0;
     int backingUpFrameCounter = 0;
-    long noBlobInFrameStartTime;
+    long noTargetInFrameStartTime;
     long backingUpStartTime;
     long searchingFrameCounter = 0;
     long searchingStartTime;
-    boolean ignoreBlobs = false;
     float staticApproachSpeed = 0.5f;
     float variableApproachSpeed = 0;
     int searchSpeed = 35;
     Random rand = new Random();
     // Random choice between -1 and 1.
     int randomSign = rand.nextBoolean() ? 1 : -1;
+    double approachTime = 3.5e9;
+    // How long to backup after landing on puck in nanoseconds.
+    double backupTime = 1e9;
+    // How long to search after backing up in nanoseconds.
+    double searchTime = 5e9;
+    // An attempt to stop the robot from flipping to the tail before searching
+    double slowDownTime = searchTime * 0.1;
 
     public void run(){
 
@@ -43,78 +47,66 @@ public class CenterPuckController extends AbcvlibController {
 
         }
         else{
-            setOutput(0, 0);
+            Log.v("centerPuck", "No pucks in sight");
 
-//            Log.v("centerblob", "No pucks in sight");
-//
-//            // Start time stamp when no puck state starts
-//            if (noBlobInFrameCounter == 0) {
-//                noBlobInFrameStartTime = System.nanoTime();
-//            }
-//
-//            noBlobInFrameCounter++;
-//            blobInFrameCounter = 0;
-//
-//            Log.v("centerblob", "No blobs. Prior to timing logic");
-//
-//            double approachTime = 3.5e9;
-//            // How long to backup after landing on puck in nanoseconds.
-//            double backupTime = 1e9;
-//            // How long to search after backing up in nanoseconds.
-//            double searchTime = 5e9;
-//            // An attempt to stop the robot from flipping to the tail before searching
-//            double slowDownTime = searchTime * 0.1;
-//
-//            // If no blob in frame for longer than approachTime...
-//            if (System.nanoTime() - noBlobInFrameStartTime > (approachTime)){
-//                // If just starting to backup
-//                if (backingUpFrameCounter == 0){
-//                    backingUpStartTime = System.nanoTime();
-//                    Log.v("centerblob", "No blobs. Setting backingupStartTime = 0");
-//                    backingUpFrameCounter++;
-//                    // Random choice between -1 and 1.
-//                    randomSign = rand.nextBoolean() ? 1 : -1;
-//                }
-//                // If backing up for more than backupTime.
-//                else if (System.nanoTime() - backingUpStartTime > backupTime) {
-//                    // Just starting to search
-//                    if (searchingFrameCounter == 0) {
-//                        searchingStartTime = System.nanoTime();
-//                        searchingFrameCounter++;
-//                    }
-//                    // Slow down to prevent flipping to tail
-//                    else if (System.nanoTime() - searchingStartTime < slowDownTime) {
-//                        setOutput(-staticApproachSpeed * 0.9, -staticApproachSpeed * 0.9);
-//                        searchingFrameCounter++;
-//                    }
-//                    // Searching for more than ignoreTurnTime but less than searchTime? Continue to search (turn).
-//                    else {
-//                        setOutput(searchSpeed * randomSign, -searchSpeed * randomSign);
-//                        searchingFrameCounter++;
-//                        ignoreBlobs = false;
-//                    }
-////                        // Searching for more than searchTime? Try backing up again.
-////                        else{
-////                            backingUpFrameCounter = 0;
-////                            searchingFrameCounter = 0;
-////                            ignoreBlobs = false;
-////                        }
-//                }
-//                // If backing up less than backupTime
-//                else{
-//                    double outputLeft = -2.0 * staticApproachSpeed;
-//                    double outputRight = outputLeft;
-//                    setOutput(outputLeft, outputRight);
-//                    backingUpFrameCounter++;
-//                }
-//            }
-//            // blob has not been in frame for less than 3 seconds. Continue Forward.
-//            else{
-//                ignoreBlobs = true;
-//                double outputLeft = staticApproachSpeed;
-//                double outputRight = staticApproachSpeed;
-//                setOutput(outputLeft, outputRight);
-//            }
+            // Start time stamp when no puck state starts
+            if (noTargetInFrameCounter == 0) {
+                noTargetInFrameStartTime = System.nanoTime();
+            }
+
+            noTargetInFrameCounter++;
+            targetInFrameCounter = 0;
+
+            Log.v("centerPuck", "No pucks. Prior to timing logic");
+
+            // If no blob in frame for longer than approachTime...
+            if ((System.nanoTime() - noTargetInFrameStartTime) > approachTime){
+                // If just starting to backup
+                if (backingUpFrameCounter == 0){
+                    backingUpStartTime = System.nanoTime();
+                    Log.v("centerPuck", "No pucks. Setting backingupStartTime = 0");
+                    backingUpFrameCounter++;
+                    // Random choice between -1 and 1.
+                    randomSign = rand.nextBoolean() ? 1 : -1;
+                }
+                // If backing up for more than backupTime.
+                else if (System.nanoTime() - backingUpStartTime > backupTime) {
+                    // Just starting to search
+                    if (searchingFrameCounter == 0) {
+                        searchingStartTime = System.nanoTime();
+                        searchingFrameCounter++;
+                    }
+                    // Slow down to prevent flipping to tail
+                    else if (System.nanoTime() - searchingStartTime < slowDownTime) {
+                        setOutput((float)(-staticApproachSpeed * 0.9), (float)(-staticApproachSpeed * 0.9));
+                        searchingFrameCounter++;
+                    }
+                    // Searching for more than ignoreTurnTime but less than searchTime? Continue to search (turn).
+                    else {
+                        setOutput(searchSpeed * randomSign, -searchSpeed * randomSign);
+                        searchingFrameCounter++;
+                    }
+//                        // Searching for more than searchTime? Try backing up again.
+//                        else{
+//                            backingUpFrameCounter = 0;
+//                            searchingFrameCounter = 0;
+//                            ignoreBlobs = false;
+//                        }
+                }
+                // If backing up less than backupTime
+                else{
+                    float outputLeft = -2.0f * staticApproachSpeed;
+                    float outputRight = outputLeft;
+                    setOutput(outputLeft, outputRight);
+                    backingUpFrameCounter++;
+                }
+            }
+            // blob has not been in frame for less than approachtime. Continue Forward.
+            else{
+                float outputLeft = staticApproachSpeed;
+                float outputRight = staticApproachSpeed;
+                setOutput(outputLeft, outputRight);
+            }
         }
     }
 
