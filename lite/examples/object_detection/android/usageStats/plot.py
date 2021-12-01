772bced6def8494d88900cd7ed8f17518690d86b
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2021-12-01 17:19:01 +0900

    More interesting stats

--- lite/examples/object_detection/android/usageStats/plot.py
+++ lite/examples/object_detection/android/usageStats/plot.py
@@ -2,6 +2,8 @@ from typing import Union, Any, List
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import scipy.stats
+from scipy.stats import linregress
 import sys
 
 # Plot BatteryLevel, State, DistanceL, DistanceR, SpeedL, SpeedR on four plots
@@ -12,11 +14,16 @@ from pandas.io.parsers import TextFileReader
 file = sys.argv[1]
 df: pd.DataFrame = pd.read_csv(file)
 
+regression = linregress(df.index[20:], df['BatteryLevel'][20:])
+print(f"R-squared: {regression.rvalue**2:.6f}")
+print(f"Slope: {regression.slope:.6f}")
+
 axes: List[plt.Axes]
 fig, axes = plt.subplots(nrows=4, ncols=1)
 
 axes[0].plot(df['BatteryLevel'])
-axes[0].set_ylim(2.8, 3.2)
+axes[0].plot(df.index[20:], (regression.intercept + regression.slope*df.index)[20:], 'r', label="linReg")
+axes[0].set_ylim(2.6, 3.2)
 axes[1].plot(df['State'])
 axes[2].plot(df['DistanceL'])
 axes[2].plot(df['DistanceR'])
