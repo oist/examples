5a9565a48dc2ea2dfe01b4894432b5c43d2331b3
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2021-12-10 17:20:25 +0900

    Added repeating song and increased generations to 10

--- lite/examples/object_detection/android/app/src/main/java/org/tensorflow/lite/examples/detection/Genes.java
+++ lite/examples/object_detection/android/app/src/main/java/org/tensorflow/lite/examples/detection/Genes.java
@@ -4,30 +4,54 @@ import android.util.Log;
 
 import com.karlotoy.perfectune.instance.PerfectTune;
 
+import org.apache.commons.math3.distribution.IntegerDistribution;
+import org.apache.commons.math3.distribution.NormalDistribution;
+
 import java.util.Arrays;
 import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 import jp.oist.abcvlib.util.ProcessPriorityThreadFactory;
 import jp.oist.abcvlib.util.ScheduledExecutorServiceWithException;
 
 public class Genes implements Runnable{
     private ScheduledExecutorServiceWithException toneExecutor = new ScheduledExecutorServiceWithException(1, new ProcessPriorityThreadFactory(Thread.NORM_PRIORITY, "TonePlayer"));
-    private int[] genes = new int[3];
-    private int g = 0;
-    private int currentGene = 0;
-    private int previousGene = 0;
+    private int[] genes = new int[10];
+    private int g = 2; // skip first two random ones
+    private int currentGene = 2;
+    private int previousGene = 1;
     private Random random = new Random();
+    private NormalDistribution normalDistribution = new NormalDistribution(5000, 1000);
     private int maxMutation = 4;
     private PerfectTune perfectTune = new PerfectTune();
     private Random rand = new Random();
     private float[] freqs = new float[16];
     private float baseFreq = 440;
+    private ScheduledFuture<?> tonePlayingFuture;
 
     public Genes(){
+        for (int i = 0; i < 3; i++){
+            genes[i] = rand.nextInt(maxMutation);
+        }
         freqs[0] = baseFreq;
         for (int i = 1; i < freqs.length - 1; i++){
             freqs[i] = freqs[i-1] * (float) Math.pow(2.0f, (1.0f / 12.0f));
         }
+        tonePlayingFuture = toneExecutor.schedule(this, 0, TimeUnit.SECONDS);
+
+        // Unit test to test mating with only one robot. Keeping commented for reference.
+
+//        Executors.newSingleThreadScheduledExecutor().scheduleWithFixedDelay(new Runnable() {
+//            @Override
+//            public void run() {
+//                exchangeGenes(Arrays.toString(genes));
+//            }
+//        }, 10, 10, TimeUnit.SECONDS);
     }
 
     public int[] parseIncomingGenes(String incomingGenes){
@@ -51,30 +75,41 @@ public class Genes implements Runnable{
         this.genes[currentGene] = genes[previousGene] + mateGenesInt[previousGene] + mutation;
         previousGene = currentGene;
 
-        toneExecutor.execute(this);
+        // Wait for a while to give mate a chance to also read QR code on self.
         try {
             Thread.sleep(4000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        // Cancel existing tone player before starting a new one
+        tonePlayingFuture.cancel(true);
+        tonePlayingFuture = toneExecutor.schedule(this, 0, TimeUnit.SECONDS);
     }
 
     @Override
     public void run() {
-        for (int gene:genes){
-            if (gene != 0){
-                // Choose freq from list of freqs
-                float freq = freqs[gene % freqs.length];
-                Log.d("Genes", "freq:" + freq);
-                perfectTune.setTuneFreq(freq);
-                perfectTune.playTune();
-                try {
-                    Thread.sleep(250);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        long songTime = 0;
+        try{
+            for (int gene:genes){
+                if (gene != 0){
+                    // Choose freq from list of freqs
+                    float freq = freqs[gene % freqs.length];
+                    Log.d("Genes", "freq:" + freq);
+                    perfectTune.setTuneFreq(freq);
+                    perfectTune.playTune();
+                    long toneTime = 250 + rand.nextInt(250); // Time to play single tone in ms varied a bit to make it less annoying
+                    songTime += toneTime;
+                    try {
+                        Thread.sleep(toneTime);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    perfectTune.stopTune();
                 }
-                perfectTune.stopTune();
             }
+        } finally {
+            tonePlayingFuture = toneExecutor.schedule(this, songTime
+                    + Math.round(normalDistribution.sample()), TimeUnit.MILLISECONDS);
         }
     }
 }
