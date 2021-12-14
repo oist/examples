b24e1a93dffb4343ac781bce3c42d7de207845b0
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2021-12-14 18:47:25 +0900

    Battery proportional speed controller balanced for mating and charging controllers

--- lite/examples/object_detection/android/app/src/main/java/org/tensorflow/lite/examples/detection/ChargeController.java
+++ lite/examples/object_detection/android/app/src/main/java/org/tensorflow/lite/examples/detection/ChargeController.java
@@ -344,8 +344,8 @@ public class ChargeController extends AbcvlibController implements WheelDataSubs
         // wheelMultipliers are an attempt to compensate for motor wear
         Log.d("Multiplier", "leftWheelMultiplier: " + leftWheelMultiplier);
         Log.d("Multiplier", "rightWheelMultiplier: " + rightWheelMultiplier);
-        left = (left / (this.batteryVoltage / 3.3f)) * leftWheelMultiplier;
-        right = (right / (this.batteryVoltage / 3.3f)) * rightWheelMultiplier;
+        left = (left / (this.batteryVoltage / 3.2f)) * leftWheelMultiplier;
+        right = (right / (this.batteryVoltage / 3.2f)) * rightWheelMultiplier;
         float finalLeft = left;
         float finalRight = right;
         Log.d("CheckStallEval", "Creating Stall Checker from charging setOutput on thread: " + Thread.currentThread().getName());
