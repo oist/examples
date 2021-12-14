2fe893b6f7195a804808c309d377bd8fbf9d470f
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2021-12-14 12:25:31 +0900

    Reorganized HighLevelControllerService for easier debugging

--- lite/examples/object_detection/android/app/src/main/java/org/tensorflow/lite/examples/detection/HighLevelControllerService.java
+++ lite/examples/object_detection/android/app/src/main/java/org/tensorflow/lite/examples/detection/HighLevelControllerService.java
@@ -72,51 +72,60 @@ public class HighLevelControllerService extends AbcvlibService implements IORead
     private UsageStats usageStats;
     private boolean shutdown = false;
     private StuckDetector stuckDetector;
+    private enum State {
+        CHARGING, MATING
+    }
 
+    // Binder given to clients
+    private final IBinder binder = new LocalBinder();
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
+    public class LocalBinder extends Binder {
+        HighLevelControllerService getService() {
+            // Return this instance of LocalService so clients can call public methods
+            return HighLevelControllerService.this;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    @Override
+    public void onCreate() {
+
+        super.onCreate();
+        countDownLatch = new CountDownLatch(1);
+        lifecycleRegistry = new LifecycleRegistry(this);
+        lifecycleRegistry.setCurrentState(Lifecycle.State.CREATED);
+        lifecycleRegistry.setCurrentState(Lifecycle.State.STARTED);
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        setIoReadyListener(this);
+        return super.onStartCommand(intent, flags, startId);
+    }
+    
     @NonNull
     @Override
     public Lifecycle getLifecycle() {
         return lifecycleRegistry;
     }
 
-    /**
-     * Allows GUI slider to set wheel multiplier in an effort to offset motor wear over time.
-     * @param value
-     */
-    public void onWheelBiasChange(float value) {
-        if (value > 0){
-            rightWheelMultiplier = 1;
-            leftWheelMultiplier = 1 + Math.abs(value);
-        }else if (value < 0){
-            leftWheelMultiplier = 1;
-            rightWheelMultiplier = 1 + Math.abs(value);
-        }else{
-            leftWheelMultiplier = 1;
-            rightWheelMultiplier = 1;
-        }
-    }
-
-    public void onMinMatingVoltageSliderChange(float value){
-        minMatingVoltage = value;
-    }
-
-    public void onMaxChargingVoltageSliderChange(float value){
-        maxChargingVoltage = value;
-    }
-
     @Override
     public double getCurrentWheelSpeed(WheelSide wheelSide) {
         return 401;
-    }
-
-    @Override
-    public void stalledShutdownRequest() {
-        Log.e("StalledShutdown", "1. Shutdown Request");
-
-        if (!shutdownRequest){
-            shutdownRequest = true;
-            cameraActivity.shutdownDialog();
-        }
     }
 
     @Override
@@ -144,6 +153,16 @@ public class HighLevelControllerService extends AbcvlibService implements IORead
         shutdown = true;
     }
 
+    @Override
+    public void stalledShutdownRequest() {
+        Log.e("StalledShutdown", "1. Shutdown Request");
+
+        if (!shutdownRequest){
+            shutdownRequest = true;
+            cameraActivity.shutdownDialog();
+        }
+    }
+
     public void restore() {
         // todo need to reset stall counts, stuck bool and free counts to zero in stuckdetector
         stuckDetector.restore();
@@ -151,51 +170,6 @@ public class HighLevelControllerService extends AbcvlibService implements IORead
         this.shutdown = false;
         getOutputs().getMasterController().addController(chargeController);
         getOutputs().getMasterController().addController(matingController);
-    }
-
-    private enum State {
-        CHARGING, MATING
-    }
-
-    // Binder given to clients
-    private final IBinder binder = new LocalBinder();
-
-    /**
-     * Class used for the client Binder.  Because we know this service always
-     * runs in the same process as its clients, we don't need to deal with IPC.
-     */
-    public class LocalBinder extends Binder {
-        HighLevelControllerService getService() {
-            // Return this instance of LocalService so clients can call public methods
-            return HighLevelControllerService.this;
-        }
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        return binder;
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        setIoReadyListener(this);
-        return super.onStartCommand(intent, flags, startId);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.P)
-    @Override
-    public void onCreate() {
-
-        super.onCreate();
-        countDownLatch = new CountDownLatch(1);
-        lifecycleRegistry = new LifecycleRegistry(this);
-        lifecycleRegistry.setCurrentState(Lifecycle.State.CREATED);
-        lifecycleRegistry.setCurrentState(Lifecycle.State.STARTED);
-    }
-
-    @Override
-    public void onStart(Intent intent, int startId) {
-        super.onStart(intent, startId);
     }
 
     /** method for clients */
@@ -237,15 +211,6 @@ public class HighLevelControllerService extends AbcvlibService implements IORead
                 }
                 break;
         }
-    }
-
-    public void setQRCodePublisher(QRCodePublisher publisher){
-        this.qrCodePublisher = publisher;
-        countDownLatch.countDown();
-    }
-
-    public void setCameraActivity(CameraActivity cameraActivity){
-        this.cameraActivity = cameraActivity;
     }
 
     public synchronized void sendControl(Detector.Recognition target_robot, Detector.Recognition target_puck){
@@ -307,8 +272,47 @@ public class HighLevelControllerService extends AbcvlibService implements IORead
         }
     }
 
+    public void analyze(int width, int height, @NonNull @NotNull int[] rgbbytes, Matrix frameToCropTransform) {
+        if (matingController != null){
+            String qrDecodedData = "";
+//            int cropSize = 320;
+//            Bitmap rgbFrameBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+//            rgbFrameBitmap.setPixels(rgbbytes, 0, width, 0, 0, width, height);
+//            Bitmap croppedBitmap = Bitmap.createBitmap(cropSize, cropSize, Bitmap.Config.ARGB_8888);
+//            final Canvas canvas = new Canvas(croppedBitmap);
+//            canvas.drawBitmap(rgbFrameBitmap, frameToCropTransform, null);
+//            int[] cropedpixels = new int[cropSize*cropSize];
+//            croppedBitmap.getPixels(cropedpixels, 0, cropSize, 0, 0, cropSize, cropSize);
+
+            RGBLuminanceSource source = new RGBLuminanceSource(width, height, rgbbytes);
+            BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(source));
+            try {
+                Result result = new QRCodeReader().decode(binaryBitmap);
+                qrDecodedData = result.getText();
+                Log.v("qrcode", "QR Code found: " + qrDecodedData);
+                matingController.onQRCodeDetected(qrDecodedData);
+            } catch (FormatException e) {
+//                Log.v("qrcode", "QR Code cannot be decoded");
+            } catch (ChecksumException e) {
+//                Log.v("qrcode", "QR Code error correction failed");
+                e.printStackTrace();
+            } catch (NotFoundException e) {
+//                Log.v("qrcode", "QR Code not found");
+            }
+        }
+    }
+
     public void setCenter(float center){
         this.center = center;
+    }
+
+    public void setQRCodePublisher(QRCodePublisher publisher){
+        this.qrCodePublisher = publisher;
+        countDownLatch.countDown();
+    }
+
+    public void setCameraActivity(CameraActivity cameraActivity){
+        this.cameraActivity = cameraActivity;
     }
 
     @RequiresApi(api = Build.VERSION_CODES.P)
@@ -374,33 +378,28 @@ public class HighLevelControllerService extends AbcvlibService implements IORead
     public void onChargerVoltageUpdate(double chargerVoltage, double coilVoltage, long timestamp) {
     }
 
-    public void analyze(int width, int height, @NonNull @NotNull int[] rgbbytes, Matrix frameToCropTransform) {
-        if (matingController != null){
-            String qrDecodedData = "";
-//            int cropSize = 320;
-//            Bitmap rgbFrameBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-//            rgbFrameBitmap.setPixels(rgbbytes, 0, width, 0, 0, width, height);
-//            Bitmap croppedBitmap = Bitmap.createBitmap(cropSize, cropSize, Bitmap.Config.ARGB_8888);
-//            final Canvas canvas = new Canvas(croppedBitmap);
-//            canvas.drawBitmap(rgbFrameBitmap, frameToCropTransform, null);
-//            int[] cropedpixels = new int[cropSize*cropSize];
-//            croppedBitmap.getPixels(cropedpixels, 0, cropSize, 0, 0, cropSize, cropSize);
-
-            RGBLuminanceSource source = new RGBLuminanceSource(width, height, rgbbytes);
-            BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(source));
-            try {
-                Result result = new QRCodeReader().decode(binaryBitmap);
-                qrDecodedData = result.getText();
-                Log.v("qrcode", "QR Code found: " + qrDecodedData);
-                matingController.onQRCodeDetected(qrDecodedData);
-            } catch (FormatException e) {
-//                Log.v("qrcode", "QR Code cannot be decoded");
-            } catch (ChecksumException e) {
-//                Log.v("qrcode", "QR Code error correction failed");
-                e.printStackTrace();
-            } catch (NotFoundException e) {
-//                Log.v("qrcode", "QR Code not found");
-            }
+    /**
+     * Allows GUI slider to set wheel multiplier in an effort to offset motor wear over time.
+     * @param value
+     */
+    public void onWheelBiasChange(float value) {
+        if (value > 0){
+            rightWheelMultiplier = 1;
+            leftWheelMultiplier = 1 + Math.abs(value);
+        }else if (value < 0){
+            leftWheelMultiplier = 1;
+            rightWheelMultiplier = 1 + Math.abs(value);
+        }else{
+            leftWheelMultiplier = 1;
+            rightWheelMultiplier = 1;
         }
+    }
+
+    public void onMinMatingVoltageSliderChange(float value){
+        minMatingVoltage = value;
+    }
+
+    public void onMaxChargingVoltageSliderChange(float value){
+        maxChargingVoltage = value;
     }
 }
